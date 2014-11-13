34e77183dea88ecf94a22117624eec90218ab37e
aquajach <aquajach@gmail.com>
2014-11-13 18:27:59 +0800

    add memcached

--- app/controllers/calendar_dates_controller.rb
+++ app/controllers/calendar_dates_controller.rb
@@ -3,10 +3,10 @@ class CalendarDatesController < ApplicationController
   def index
     # flash[:notice] = 'Welcome to my section!'
     current_user = User.first
-    @calendar_dates = current_user.calendar_dates.includes(:events).select(:date, :id)
-    count = current_user.calendar_dates.count
-    max_updated_at = [current_user.events.maximum(:updated_at), current_user.calendar_dates.maximum(:updated_at)].max
-    fresh_when etag: current_user.calendar_dates, last_modified: max_updated_at
+    @calendar_dates = current_user.calendar_dates.includes(:events).select(:date, :id, :updated_at)
+    # count = current_user.calendar_dates.count
+    # max_updated_at = [current_user.events.maximum(:updated_at), current_user.calendar_dates.maximum(:updated_at)].max
+    # fresh_when etag: current_user.calendar_dates, last_modified: max_updated_at
     # fresh_when [{etag: current_user.calendar_dates, last_modified: max_updated_at}, form_authenticity_token]
     # expires_in 3600
     # respond_with @calendar_dates if stale?(current_user)
